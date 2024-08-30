95e67d0e132bdb15d0516a27108a1079191d0e1c
Mr. Skid <jordanflamesbusiness+1@gmail.com>
2024-08-30 02:05:53 +0100

    Update __init__.py

--- Helper/__init__.py
+++ Helper/__init__.py
@@ -1,1 +1,54 @@
-#placeholder
+__title__ = 'SkidRaider'
+__author__ = 'Mr.Skid'
+__copyright__ = 'discord.gg/pornhub'
+__version__ = '1.0.0'
+
+#took imports from nexusmulti tool thanks vatos.
+import re
+import os
+import time
+import threading
+import requests
+import concurrent.futures
+import json
+import urllib.request
+import socket
+import base64 
+import codecs
+import random
+import string
+import httpx
+import tls_client
+import secrets
+import ctypes
+import websocket
+import sys
+import aiohttp
+import asyncio
+import pytz
+import shutil
+import tempfile
+import subprocess
+import http
+
+import undetected_chromedriver as uc
+import tkinter as tk
+
+from tkinter import filedialog
+from faker import Faker
+from concurrent.futures import ThreadPoolExecutor
+from colorama import Fore, Style
+from datetime import datetime
+from tls_client import Session
+from pathlib import Path
+from base64 import b64decode
+from Crypto.Cipher import AES
+from win32crypt import CryptUnprotectData
+from os import getlogin, listdir
+from json import loads
+from re import findall
+from os.path import isfile, join
+from bs4 import BeautifulSoup
+from pytube import YouTube
+from urllib.parse import urlparse, urljoin
+from urllib.request import urlretrieve
