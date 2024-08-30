54e5f1835b77d3a85e2c259171bafc3131ac711f
Mr. Skid <jordanflamesbusiness+1@gmail.com>
2024-08-30 02:06:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-#placeholder
 #for the record i do know how to code im just doing this for fun dont take this seriously all credits are given.
+
+from Helper import *
+from Helper.Common.utils import *
